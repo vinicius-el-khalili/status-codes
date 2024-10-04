5f3b551b9f867c84cc7c69e6591cb1410ccc4334
vini <velkhaliliborsato@gmail.com>
2024-10-04 02:57:27 -0300

    feat: api & landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,31 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+        <h1>a dead simple api that returns status codes</h1>
         <ol>
           <li>
-            Get started by editing <code>app/page.tsx</code>.
+            make requests at <code>{`https://status-codes.vercel.app/api/status/{statusCode}`}</code>
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>here's a list of accepted status codes:<br/>
+            {`"100", "101", "102", "103", "200", "201", "202", "203", "204", "205", "206", "207", "208", "214", "226",
+        "300", "301", "302", "303", "304", "305", "307", "308", "400", "401", "402", "403", "404", "405", "406", "407",
+        "408", "409", "410", "411", "412", "413", "414", "415", "416", "417", "418", "420", "421", "422", "423", "424",
+        "425", "426", "428", "429", "431", "444", "450", "451", "497", "498", "499", "500", "501", "502", "503", "504",
+        "506", "507", "508", "509", "510", "511", "521", "522", "523", "525", "530", "599"`}
+          </li>
+          <li>
+            if your status code doesn't belong in this list, you'll get 404
+          </li>
         </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <Link href={'https://github.com/vinicius-el-khalili/status-codes'} target="_black" >
+            {`github ->`}
+        </Link>
+      </main> 
     </div>
   );
 }
